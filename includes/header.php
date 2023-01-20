35863de09f0879dd4a46610ac85aaba0e4235d25
Felipe Parreira <107953342+felparreira@users.noreply.github.com>
2023-01-20 17:57:01 -0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -8,10 +8,9 @@
         <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">      -->                             
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="../js/main.js"></script>
-    </head>
-    <title>Flip Card</title>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>        
+        <title>Flip Card</title>
+    </head>    
     <body>
         <div class="main_header">
             <img src="/img/logo.png">            
